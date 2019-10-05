78e51c0aacef417a833d3df0f28dfdc4342c3d26
黒瀬友太 <kuroseyuuta@mba.toshima.ne.jp>
2019-10-06 02:42:42 +0900

    add _id.vue for nuxt-routing

---
+++ pages/users/_id.vue
@@ -0,0 +1,20 @@
+<template>
+  <section>
+    <h1>{{ message }}</h1>
+    <p>id: {{ this.$route.params.id }}</p>
+  </section>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      message: "/users/_id.vueを表示中"
+    }
+  },
+  validate({ params }) {
+    // 数値でない場合にエラーとする（正規表現で数値のみとしている）
+    return /^\d+$/.test(params.id)
+  }
+}
+</script>
